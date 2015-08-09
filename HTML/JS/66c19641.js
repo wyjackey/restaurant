264e42211e258eb1776bb23b77a12d85dbfd2722
Steve Wu <shoukuiwu@yahoo.ca>
2015-08-08 20:47:06 -0400

    still working on repots

--- HTML/JS/66c19641.js
+++ HTML/JS/66c19641.js
@@ -1,1 +1,50 @@
-!function(){window.CDNReady=!0}();
+
+
+! function getreport() {
+				var line1 = [[7, '菜品1'], [9, '菜品2'], [15, '菜品3'], [12, '菜品4'], [4, '菜品5'], [3, '菜品6'], [10, '菜品7']];
+				var line2 = [0, 0];
+				var titles = $("input[name='sales']:checked").next('label').text();
+				for (var i = 0; i < line1.length; i++) {
+					line3 = [line1[i][1], line1[i][0]];
+
+					line2.unshift(line3);
+				}
+				var plot1 = $.jqplot('chart1', [line1], {
+					title : titles,
+					seriesDefaults : {
+						renderer : $.jqplot.BarRenderer,
+						shadowAngle : 135,
+						rendererOptions : {
+							barDirection : 'horizontal'
+						},
+						pointLabels : {
+							show : true
+						}
+					},
+
+					axes : {
+						yaxis : {
+							renderer : $.jqplot.CategoryAxisRenderer
+						}
+					}
+				});
+				$('#chart1').bind('jqplotDataHighlight', function(ev, seriesIndex, pointIndex, data) {
+					$('#info2').html('菜品: ' + line1[pointIndex][1] + ', 排名: ' + data[1] + ', 销量: ' + data[0]);
+				});
+
+				
+
+				var plot2 = $.jqplot('pieChart', [line2], {
+					seriesDefaults : {
+						renderer : $.jqplot.PieRenderer,
+						rendererOptions : {
+							showDataLabels : true
+						}
+					},
+					legend : {
+						show : true,
+						location : 'e'
+					}
+				});
+
+			});
